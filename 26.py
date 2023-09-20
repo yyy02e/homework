5228755e6f8adb7b1e4ab81aa2f25fd51a5ba65d
Scyyyyy <144771495+yyy02e@users.noreply.github.com>
2023-09-20 17:22:43 +0800

    Add files via upload

---
+++ 26.py
@@ -0,0 +1,9 @@
+def newton(x):
+    min = 0.0000001
+    guess = x/2
+    #guess = x/4
+    while abs(guess ** 2 - x) >= min:
+        guess = guess - (((guess ** 2)-x) / (guess * 2))
+    return guess
+print(newton(2))
+#有影响
