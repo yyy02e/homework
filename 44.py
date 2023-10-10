1c9bbc032eccecf3684fe23b5c0b1faf2dd3744c
Cheng <13611872105@163.com>
2023-10-10 19:27:04 +0800

    4

---
+++ 44.py
@@ -0,0 +1,16 @@
+def shellsort(nums):
+    step = len(nums)//2
+    while step > 0:
+        for i in range(step, len(nums)):
+            index = i
+            while index >= step and nums[index-step] > nums[index]:
+                nums[index], nums[index - step] = nums[index - step], nums[index]
+                index -= step
+        step //= 2
+    print(nums)
+
+nums = []
+for i in range(0,8):
+    a = int(input())
+    nums.append(a)
+shellsort(nums)
